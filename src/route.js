4faf14d25dc30a9d9c6428e7aa574f371bdb4bcb
Daniel070793 <daniel070793@gmail.com>
2017-10-23 12:28:13 +0200

    added captcha check

--- src/route.js
+++ src/route.js
@@ -21,4 +21,13 @@ express.get("/2", (req, res) => {
 	res.sendFile("./pages/basicLoginCaptcha.html", { root: __dirname });
 });
 
+express.post("/2", parser, (req, res) => {
+	if (req.body.uname == user.username && req.body.psw == user.password && req.body['g-recaptcha-response'] !== "" || req.body['g-recaptcha-response'] !== null || req.body['g-recaptcha-response'] !== undefined) {
+		res.sendFile("./pages/loginSuccess.html", { root: __dirname });
+	} if
+		(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === "" || req.body['g-recaptcha-response'] === null) {
+		return res.json({ "responseCode": 1, "responseDesc": "Please select captcha" });
+	}
+});
+
 module.exports = express;
