d2a6f722b18975cd0a05355370bf84652e707049
RamananSri <1058515@ucn.dk>
2017-10-23 10:43:20 +0200

    form styling

--- src/route.js
+++ src/route.js
@@ -1,7 +1,9 @@
 const express = require("express").Router();
 const bodyParser = require("body-parser");
+bodyParser.urlencoded();
 
 express.get("/", (req, res) => {
+	console.log(req.body.uname);
 	res.sendFile("./pages/basicLoginForm.html", { root: __dirname });
 });
 
