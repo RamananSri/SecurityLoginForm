9b7d44f9c9359590fddda195ebd0a05fac949f29
RamananSri <1058515@ucn.dk>
2017-10-23 10:20:11 +0200

    added express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const route = require("./src/pages/basicLoginForm");
+const app = express();
+
+app.use("/login", route);
+
+app.listen(3000, () => {
+	console.log("server running");
+});
